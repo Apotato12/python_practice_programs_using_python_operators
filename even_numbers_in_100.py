27dab63da195386e27576998b379d9fb1e820d17
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-12 12:30:45 +0800

    added_PEP8_Format_and_comments

--- even_numbers_in_100.py
+++ even_numbers_in_100.py
@@ -1,3 +1,12 @@
-for number in range(0, 101):
-    if number % 2 == 0:
-        print(number)
+#Program 9
+#Atienza,Rein Gabriel
+#BSCPE 1-2
+def evens_in_100():
+    """"prints the even values in 1 to 100"""
+    for number in range(0, 101):
+
+        if number % 2 == 0:
+         
+         print(number)
+
+evens_in_100()
