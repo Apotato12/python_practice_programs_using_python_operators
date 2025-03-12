e88eb5b351b8afcd73d73c857af6d2b1aa60d443
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-12 10:30:55 +0800

    followed_PEP8_standards

--- bigger_number_determiner.py
+++ bigger_number_determiner.py
@@ -1,4 +1,8 @@
-num1 = float(input("Enter first number: ")) 
-num2 = float(input("Enter second number: "))
-
-print(max(num1, num2),"is the larger number" )
+def get_the_larger_of_two_numbers():
+     """allows the user to input two numbers and print the one with the higher value."""
+     num1 = float(input("Enter first number: "))
+     num2 = float(input("Enter second number: "))
+ 
+     print(max(num1, num2),"is the larger number" )
+ 
+get_the_larger_of_two_numbers()
