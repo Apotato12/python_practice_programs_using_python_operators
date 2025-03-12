173c540094d6445bfb199d00f90fe261afe2408a
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-12 12:16:46 +0800

    added_PEP8_standards_and_comments

--- odd_numbers_within_10_numbers_determiner.py
+++ odd_numbers_within_10_numbers_determiner.py
@@ -1,9 +1,17 @@
-odd_count = 0
+#Program 8
+#Atienza,Rein Gabriel
+#BSCPE 1-2
+""""starts the value of odss to 0 to hold the other values given and then adds 1 to it when and odd number is"""
+def Odd_numbers_within_10():
 
-for i in range(10):
-    number = float(input(f"Enter number {i + 1}: ")) 
+    odd_numbers = 0
+
+    for i in range(10):
+        number = float(input(f"Enter number {i + 1}: ")) 
     
-    if number % 2 != 0:
-        odd_count += 1 
+        if number % 2 != 0:
+            odd_numbers += 1 
 
-print(f"The number of odd numbers entered is: {odd_count}")
+    print("The number of odd numbers entered is:", odd_numbers2)
+
+Odd_numbers_within_10()
