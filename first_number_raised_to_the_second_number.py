79e9faeb83a18b1627f992cf1b8a59ca95a3f457
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-12 11:22:34 +0800

    pseudocode

---
+++ first_number_raised_to_the_second_number.py
@@ -0,0 +1,4 @@
+#Asks the user to input 2 numbers
+#the program then uses exponentiation on the 2 numbers with num 1 being the first and num 2 being the second 
+#the program then multiplies num 1 with itself based on the amount that is given and that amount will be num 2
+#after the multiplication print the result
