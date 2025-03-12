90bbec4f0c6955c4c6e7718d5e8b8f4ca1a7144c
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-12 12:05:19 +0800

    working_code

--- odd_numbers_within_10_numbers_determiner.py
+++ odd_numbers_within_10_numbers_determiner.py
@@ -1,3 +1,9 @@
-#prompts the user to input 10 numbers
-#makes the program determine which numbers are odd
-#print the odd numbers
+odd_count = 0
+
+for i in range(10):
+    number = float(input(f"Enter number {i + 1}: ")) 
+    
+    if number % 2 != 0:
+        odd_count += 1 
+
+print(f"The number of odd numbers entered is: {odd_count}")
