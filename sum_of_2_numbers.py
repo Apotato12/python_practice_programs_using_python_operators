22f70995ba91ec63f453582fc40f616d0f29e3bd
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-12 10:45:55 +0800

    followed_PEP8_standards_and_added_comments

--- sum_of_2_numbers.py
+++ sum_of_2_numbers.py
@@ -1,6 +1,13 @@
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+#Program 2
+#Atienza,Rein Gabriel
+#BSCPE 1-2
+def adds_the_2_numbers_and_extracts_their_sum():
+     """prompts the user to input two numbers then add the numbers then prints their sum"""
+     num1 = float(input("Enter the first number: "))
+     num2 = float(input("Enter the second number: "))
 
-sum = num1 + num2
+     sum = num1 + num2
 
-print("the sum of 2 numbers is:", sum)
+     print("the sum of 2 numbers is:", sum)
+
+adds_the_2_numbers_and_extracts_their_sum()
