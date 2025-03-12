469616d35854fda43e84f105697f899ecf4b2289
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-12 11:45:16 +0800

    followed_PEP8_standards_and_added_comments

--- input_10_numbers_and_get_their_sum.py
+++ input_10_numbers_and_get_their_sum.py
@@ -1,8 +1,14 @@
-sum = 0
+#Program 7
+#Atienza,Rein Gabriel
+#BSCPE 1-2
+def get_10_numbers_and_their_sum():
+    """"starts the value of sum to 0 to hold the other values given and then adds them with each other"""
+    sum = 0
 
-for i in range(10):
-    number = float(input(f"Enter number {i + 1}: "))  # Prompt for each number
-    sum += number  # Add the number to the total sum
+    for i in range(10):
+        number = float(input(f"Enter number {i + 1}: "))  # Prompt for each number
+        sum += number  # Add the number to the total sum
 
-# Print the sum of all the numbers
-print(f"The sum of all the numbers is:", sum)
+        print(f"The sum of all the numbers is:", sum)
+
+get_10_numbers_and_their_sum()
