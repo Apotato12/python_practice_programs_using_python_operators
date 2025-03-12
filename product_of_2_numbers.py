859c8942b21fc67c0bcc429b7f855a4dbb7c9ef6
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-12 10:58:53 +0800

    added_PEP8_standards_and_comments

--- product_of_2_numbers.py
+++ product_of_2_numbers.py
@@ -1,6 +1,13 @@
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+#Program 2
+#Atienza,Rein Gabriel
+#BSCPE 1-2
+def multiply_the_number_with_each_other():
+    """prompts the user to enter 2 numbers and then multiplies those numbers with each other"""
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
  
-product = num1 * num2
+    product = num1 * num2
  
-print("the product of 2 numbers is:", product)
+    print("the product of 2 numbers is:", product)
+
+multiply_the_number_with_each_other()
