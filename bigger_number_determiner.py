e64f107780ea91863513206507aef67a92aa49ff
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-12 10:36:17 +0800

    added_comments

--- bigger_number_determiner.py
+++ bigger_number_determiner.py
@@ -1,3 +1,6 @@
+#Program 1
+#Atienza,Rein Gabriel
+#BSCPE 1-2
 def get_the_larger_of_two_numbers():
      """allows the user to input two numbers and print the one with the higher value."""
      num1 = float(input("Enter first number: "))
