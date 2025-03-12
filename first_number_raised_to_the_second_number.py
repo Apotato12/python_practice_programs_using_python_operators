049ac08b91988a7c8ab0ad225dd9dee026626246
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-12 11:30:44 +0800

    added_PEP8_standards_and_comments

--- first_number_raised_to_the_second_number.py
+++ first_number_raised_to_the_second_number.py
@@ -1,6 +1,13 @@
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+#Program 2
+#Atienza,Rein Gabriel
+#BSCPE 1-2
+def raise_num1_to_num2():
+    """"prompts the user to enter 2 numbers after entering 2 numbers raise num 1 with num 2"""
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
 
-raised = num1 ** num2
+    raised = num1 ** num2
 
-print ("num1 raised to num 2 is", raised)
+    print ("num1 raised to num 2 is", raised)
+
+raise_num1_to_num2
