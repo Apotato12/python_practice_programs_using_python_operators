d79e53925c327c02d472edcc1e9526068e80ef1b
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-12 10:35:12 +0800

    followed_pep8_standards

--- equal_value_determiner.py
+++ equal_value_determiner.py
@@ -1,5 +1,12 @@
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+#Program 2
+#Atienza,Rein Gabriel
+#BSCPE 1-2
+def determine_if_the_numbers_are_of_equal_value():
+    """allows the user to input two numbers then check if the numbers are equal if they are print num1 and num2 are equal."""
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
 
-if num1 == num2:
-    print(str(num1) + " and " + str(num2) + " are equal.")
+    if num1 == num2:
+        print("the numbers are equal")
+
+determine_if_the_numbers_are_of_equal_value()
