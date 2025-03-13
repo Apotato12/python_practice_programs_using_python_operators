34afc383fcf73c2a3e104a6711d235bdc98b456e
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-13 20:38:22 +0800

    working_code

--- highest_to_lowest.py
+++ highest_to_lowest.py
@@ -1,5 +1,18 @@
-#Ask the user to input a number
-#continue asking until the user inputs and invalid number
-#create a set
-#sort the set from highest to lowest
-#print the set
+highest_to_lowest = set() 
+
+while True:
+        user_input = input("Enter a number (or type something invalid to stop): ")
+        
+        try:
+            num = float(user_input)
+            highest_to_lowest.add(num)
+        except ValueError:
+            print("Invalid input. Exiting the program.")
+            break
+
+
+if highest_to_lowest:  
+        sorted_highest_to_lowest = sorted(highest_to_lowest, reverse=True)
+        print("")
+        for number in sorted_highest_to_lowest:
+            print(number)
