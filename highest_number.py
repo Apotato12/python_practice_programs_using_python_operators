ed7f0d1da134da25f7563543e2c00661e9dca780
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-13 20:25:54 +0800

    pseudocode

--- highest_number.py
+++ highest_number.py
@@ -1,17 +1,5 @@
-highest_number = set()
-
-while True:
-        user_input = input("Enter a number (or type something invalid to stop): ")
-        
-        try:
-            num = float(user_input)  # Convert input to float
-            highest_number.add(num)  # Add the number to the set
-        except ValueError:
-            print("Invalid input. Exiting the program.")
-            break
-        
-if highest_number:
-        highest_number = max(highest_number)
-        print("The highest number entered is:", highest_number)
-else:
-        print("No valid highest_number were entered.")
+#prompt the user to input numbers 
+#contiue asking until the user inputs an invalid number
+#create a set
+#determine which number has the highest value
+#print that number
