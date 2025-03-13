c0733c07c581c4ef7c56c7676ae81f744ce0cddc
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-13 20:40:21 +0800

    followed_PEP8_and_added_comments

--- highest_to_lowest.py
+++ highest_to_lowest.py
@@ -1,18 +1,27 @@
-highest_to_lowest = set() 
+# Program 4 Batch 2
+# Atienza, Rein Gabriel
+# BSCPE 1-2
 
-while True:
+def sort_numbers_highest_to_lowest():
+    """Prompt the user to input numbers and display them from highest to lowest."""
+    highest_to_lowest = set()
+
+    while True:
         user_input = input("Enter a number (or type something invalid to stop): ")
         
         try:
             num = float(user_input)
-            highest_to_lowest.add(num)
+            highest_to_lowest.add(num) 
         except ValueError:
             print("Invalid input. Exiting the program.")
             break
 
-
-if highest_to_lowest:  
+    if highest_to_lowest:  
         sorted_highest_to_lowest = sorted(highest_to_lowest, reverse=True)
-        print("")
+        print("\nNumbers from highest to lowest:")
         for number in sorted_highest_to_lowest:
             print(number)
+    else:
+        print("No valid numbers were entered.")
+
+sort_numbers_highest_to_lowest()
