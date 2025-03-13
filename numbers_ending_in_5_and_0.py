edbc82fd932e57f7b66f93b68601111c92f3b7d6
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-13 14:50:58 +0800

    followed_PEP8_standards_and_added_comments

--- numbers_ending_in_5_and_0.py
+++ numbers_ending_in_5_and_0.py
@@ -1,3 +1,11 @@
-for number in range(101):
-    if number % 10 == 0 or number % 10 == 5:
-     print(number)
+# Program 9 Batch 2
+# Atienza, Rein Gabriel
+# BSCPE 1-2
+
+def numbers_ending_with_0_or_5():
+    """Print numbers from 0 to 100 that end with 0 or 5."""
+    for number in range(101):
+        if number % 10 == 0 or number % 10 == 5:
+            print(number)
+
+numbers_ending_with_0_or_5()
