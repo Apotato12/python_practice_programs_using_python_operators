4d4f61ff04f23db6b8a7981ff99a51a0db39882a
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-13 13:20:13 +0800

    working_code

--- first_number_minus_the_rest.py
+++ first_number_minus_the_rest.py
@@ -1,4 +1,8 @@
-#ask the user to input 10 numbers
-#seperate the first number from the rest
-#add the rest of the numbers together then minus it with the first number
-#print the result
+numbers = [float(input(f"Enter number {i + 1}: ")) for i in range(10)]
+
+first_number = numbers[0]
+rest_of_numbers = numbers[1:]
+
+result = first_number - sum(rest_of_numbers)
+
+print("Result:", result)
