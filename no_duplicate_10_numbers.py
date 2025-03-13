735fce2bc132e150b0dc8fc1d2155b568bcd014e
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-13 17:54:44 +0800

    followed_PUP8_format_and_added_comments

--- no_duplicate_10_numbers.py
+++ no_duplicate_10_numbers.py
@@ -1,19 +1,25 @@
-list_of_numbers = []
+# Program 1 Batch 3
+# Atienza, Rein Gabriel
+# BSCPE 1-2
+def find_non_repeating_numbers():
+    """Ask the user to input 10 numbers and display those without duplicates."""
+    list_of_numbers = []
 
-print("enter 10 numbers:")
-for i in range(10):
-    num = float(input("Number " + str(i + 1)+ ": "))
-    list_of_numbers.append(num)
+    print("Enter 10 numbers:")
+    for i in range(10):
+        num = float(input("Number " + str(i + 1) + ": "))
+        list_of_numbers.append(num)
 
-count = {}
-for num in list_of_numbers:
-    if num in count:
-        count[num] = count[num] + 1 
-    else:
-        count[num] = 1
+    count = {}
+    for num in list_of_numbers:
+        if num in count:
+            count[num] = count[num] + 1 
+        else:
+            count[num] = 1
 
-print("Numbers without duplicates:")
-for num in count:
-    if count[num] == 1:
-        print(num)
-        
+    print("Numbers without duplicates:")
+    for num in count:
+        if count[num] == 1:
+            print(num)
+
+find_non_repeating_numbers()
