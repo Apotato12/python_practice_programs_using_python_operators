1ddce674647a920674fa1eaf5cccd1d25d942c80
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-13 08:29:52 +0800

    pseudocode

---
+++ smaller_of_2_numbers.py
@@ -0,0 +1,5 @@
+#asks the user to input 2 numbers
+#the program then determines which number is smaller
+#if num1 is smaller print num1 is the smaller number
+#if num2 is the smaller number print num2 is the smaller number
+#if no number is smaller than the other print no number is smaller than the other
