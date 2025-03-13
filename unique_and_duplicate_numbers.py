fe20cd80c14332b707470d3fb44a64985835006f
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-13 18:41:26 +0800

    followed_PEP8_guidelines_and_added_comments

--- unique_and_duplicate_numbers.py
+++ unique_and_duplicate_numbers.py
@@ -1,23 +1,30 @@
-duplicate_numbers = set()
-unique_numbers = set()
+# Program 3 Batch 2
+# Atienza, Rein Gabriel
+# BSCPE 1-2
+def track_numbers():
+    """Continuously ask the user for a number and track unique and duplicate entries."""
+    duplicate_numbers = set()
+    unique_numbers = set()
 
-while True:
-    user_input = input("Enter a number (or type something invalid to stop): ")
-    
-    try:
-        num = float(user_input) 
-        if num in unique_numbers:
-            duplicate_numbers.add(num)
-        else:
-            unique_numbers.add(num)
-    except ValueError:
-        print("Invalid input. Exiting the program.")
-        break
+    while True:
+        user_input = input("Enter a number (or type something invalid to stop): ")
+        
+        try:
+            num = float(user_input) 
+            if num in unique_numbers:
+                duplicate_numbers.add(num)
+            else:
+                unique_numbers.add(num)
+        except ValueError:
+            print("Invalid input. Exiting the program.")
+            break
 
-print("Unique numbers entered:")
-for number in unique_numbers:
-    print(number)
+    print("Unique numbers entered:")
+    for number in unique_numbers:
+        print(number)
 
-print("Duplicate numbers entered:")
-for number in duplicate_numbers:
-    print(number)
+    print("Duplicate numbers entered:")
+    for number in duplicate_numbers:
+        print(number)
+
+track_numbers()
