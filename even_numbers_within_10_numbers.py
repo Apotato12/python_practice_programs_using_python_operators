db1b823ea8b897f2c83f5f78d85aaf3821d9214a
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-13 13:45:14 +0800

    added_PEP8_format_and_comments

--- even_numbers_within_10_numbers.py
+++ even_numbers_within_10_numbers.py
@@ -1,8 +1,15 @@
-numbers = [0] * 10
+#Program 7 Batch 2
+#Atienza,Rein Gabriel
+#BSCPE 1-2
+def even_numbers_within_10_inputs():
+    """"Prompts the user to enter 10 numbers then check which are the even numbers then prints them"""
+    numbers = [0] * 10
 
-for i in range(10):
-    numbers[i] = float(input(f"Enter number {i + 1}: "))
+    for i in range(10):
+        numbers[i] = float(input(f"Enter number {i + 1}: "))
 
-even_numbers = [num for num in numbers if num % 2 == 0]
+    even_numbers = [num for num in numbers if num % 2 == 0]
 
-print("Count of even numbers:", even_numbers)
+    print("Count of even numbers:", even_numbers)
+
+even_numbers_within_10_inputs()
