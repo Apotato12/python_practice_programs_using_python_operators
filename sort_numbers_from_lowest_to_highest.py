13030ec87948906a20c7562b67e62067dc1dd303
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-13 19:18:04 +0800

    followed_PEP8_format_and_added_comments

--- sort_numbers_from_lowest_to_highest.py
+++ sort_numbers_from_lowest_to_highest.py
@@ -1,5 +1,10 @@
-numbers = set()
-while True:
+# Program 5 Batch 3
+# Atienza, Rein Gabriel
+# BSCPE 1-2
+def sort_numbers():
+    """Continuously ask the user for a number and display the numbers from lowest to highest."""
+    numbers = set()
+    while True:
         user_input = input("Enter a number (or type something invalid to stop): ")
         try:
             num = float(user_input)
@@ -8,8 +13,10 @@ while True:
             print("Invalid input. Exiting the program.")
             break
 
-sorted_numbers = sorted(numbers)
+    sorted_numbers = sorted(numbers)
 
-print("Numbers from lowest to highest:")
-for number in sorted_numbers:
-    print(number)
+    print("Numbers from lowest to highest:")
+    for number in sorted_numbers:
+        print(number)
+
+sort_numbers()
