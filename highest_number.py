b0209e2fd2a4ad0ea100bc20f1f99f95cd29f5e0
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-13 20:26:30 +0800

    working_code

--- highest_number.py
+++ highest_number.py
@@ -1,5 +1,17 @@
-#prompt the user to input numbers 
-#contiue asking until the user inputs an invalid number
-#create a set
-#determine which number has the highest value
-#print that number
+highest_number = set()
+
+while True:
+        user_input = input("Enter a number (or type something invalid to stop): ")
+        
+        try:
+            num = float(user_input)  # Convert input to float
+            highest_number.add(num)  # Add the number to the set
+        except ValueError:
+            print("Invalid input. Exiting the program.")
+            break
+        
+if highest_number:
+        highest_number = max(highest_number)
+        print("The highest number entered is:", highest_number)
+else:
+        print("No valid highest_number were entered.")
