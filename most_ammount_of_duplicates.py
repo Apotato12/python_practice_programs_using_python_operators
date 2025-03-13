bcbbbb0a84a614786a75961d66e7520a2cb3e035
Atienza, Rein Gabriel <atienza.reingabriel308129@gmail.com>
2025-03-13 20:05:04 +0800

    pseudocode

---
+++ most_ammount_of_duplicates.py
@@ -0,0 +1,5 @@
+#ask the user to input numbers
+#create a set
+#check if a number that is on the set has been entered already
+#count the number of times it's on the set
+#print the one with the most amount of times counted
