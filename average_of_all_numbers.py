95064fe4e996db47727af75455cf491dc687cba9
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-13 20:47:59 +0800

    added_PEP8_guidelines_and_added_comments

--- average_of_all_numbers.py
+++ average_of_all_numbers.py
@@ -1,15 +1,23 @@
-average_numbers = set()
+# Program to calculate the average of user-input numbers
+# Atienza, Rein Gabriel
+# BSCPE 1-2
 
-while True:
-    user_input = input("Enter a number (or type something invalid to stop): ")
+def calculate_average():
+    """Prompt the user to input numbers and display the average of those numbers."""
+    average_numbers = set()
+
+    while True:
+        user_input = input("Enter a number (or type something invalid to stop): ")
         
-    try:
-        num = float(user_input)
-        average_numbers.add(num)
-    except ValueError:
-        print("Invalid input. Exiting the program.")
-        break
+        try:
+            num = float(user_input)
+            average_numbers.add(num)
+        except ValueError:
+            print("Invalid input. Exiting the program.")
+            break
 
-if average_numbers:  
-    average = sum(average_numbers) / len(average_numbers) 
-    print("The average of the entered numbers is:", average)
+    if average_numbers:  
+        average = sum(average_numbers) / len(average_numbers)
+        print("The average of the entered numbers is:", average)
+        
+calculate_average()
