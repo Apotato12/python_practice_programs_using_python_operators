49c4c89706bed8f4c3d94c65746fa58dc438a19c
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-13 08:38:19 +0800

    followed_PEP8_Guidelines_and_added_comments

--- smaller_of_2_numbers.py
+++ smaller_of_2_numbers.py
@@ -1,9 +1,16 @@
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+#Program 1 batch 2
+#Atienza,Rein Gabriel
+#BSCPE 1-2
+def determine_smaller_number():
+    """Ask the user to input 2 numbers and determine which one is smaller."""
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
 
-if num1 < num2:
-    print (num1,"is the smaller number" )
-elif num2 < num1 :
-    print (num2, "is the smaller number")
-else:
-    print ("no number is smaller than the other")
+    if num1 < num2:
+        print(num1, "is the smaller number")
+    elif num2 < num1:
+        print(num2, "is the smaller number")
+    else:
+        print("No number is smaller than the other.")
+
+determine_smaller_number()
