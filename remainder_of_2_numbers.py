1bb0b5375038463ce07e7e850fe345ee3c93f5c3
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-13 13:08:02 +0800

    followed_PEP8_standard_and_added_comments

--- remainder_of_2_numbers.py
+++ remainder_of_2_numbers.py
@@ -1,6 +1,13 @@
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+#Program 5 batch 2
+#Atienza,Rein Gabriel
+#BSCPE 1-2
+def calculate_remainder():
+    """Ask the user to input 2 numbers then divide the first number with the second number then print the remainder"""
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
 
-remainder = num1 % num2
+    remainder = num1 % num2
 
-print ("the remainder is:", remainder)
+    print ("the remainder is:", remainder)\
+
+calculate_remainder()
