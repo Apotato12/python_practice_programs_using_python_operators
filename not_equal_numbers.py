dee1b2e4530cd4795bd8cba3e0e41a75369f492f
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-13 08:57:52 +0800

    followed_PEP8_guidelines_and_added_comments

--- not_equal_numbers.py
+++ not_equal_numbers.py
@@ -1,7 +1,14 @@
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+#Program 2 Batch 2
+#Atienza,Rein Gabriel
+#BSCPE 1-2
+def determine_if_numbers_are_not_equal():
+    """Ask the user for 2 numbers and determine if they are equal or not."""
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
 
-if num1 != num2:
-    print ("num1 and num2 are not equal")
-else:
-    print("that value is equal")
+    if num1 != num2:
+        print("The numbers are not equal.")
+    else:
+        print("The numbers are equal.")
+
+determine_if_numbers_are_not_equal()
