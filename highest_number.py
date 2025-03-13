f0e83d4d8b12271e4b21cfd0d0e2dbbf3330ec92
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-13 20:28:53 +0800

    followed_PEP8_and_added_comments

--- highest_number.py
+++ highest_number.py
@@ -1,17 +1,25 @@
-highest_number = set()
+# Program 3 Batch 4
+# Atienza, Rein Gabriel
+# BSCPE 1-2
 
-while True:
+def find_highest_number():
+    """Prompt the user to input numbers and display the highest number entered."""
+    highest_numbers = set() 
+
+    while True:
         user_input = input("Enter a number (or type something invalid to stop): ")
         
         try:
-            num = float(user_input)  # Convert input to float
-            highest_number.add(num)  # Add the number to the set
+            num = float(user_input)
+            highest_numbers.add(num)
         except ValueError:
             print("Invalid input. Exiting the program.")
             break
-        
-if highest_number:
-        highest_number = max(highest_number)
+
+    if highest_numbers:
+        highest_number = max(highest_numbers) 
         print("The highest number entered is:", highest_number)
-else:
-        print("No valid highest_number were entered.")
+    else:
+        print("No valid numbers were entered.")
+
+find_highest_number()
