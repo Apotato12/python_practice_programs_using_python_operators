d6070354c199cf1a750ad2e835410aca936f2277
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-13 08:30:58 +0800

    working_code

--- smaller_of_2_numbers.py
+++ smaller_of_2_numbers.py
@@ -1,5 +1,9 @@
-#asks the user to input 2 numbers
-#the program then determines which number is smaller
-#if num1 is smaller print num1 is the smaller number
-#if num2 is the smaller number print num2 is the smaller number
-#if no number is smaller than the other print no number is smaller than the other
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+if num1 < num2:
+    print (num1,"is the smaller number" )
+elif num2 < num1 :
+    print (num2, "is the smaller number")
+else:
+    print ("no number is smaller than the other")
