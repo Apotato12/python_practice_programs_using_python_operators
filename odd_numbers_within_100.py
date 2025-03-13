4ac57946f5c98e8cd167173c5190585ac75c2a54
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-13 14:37:20 +0800

    followed_PEP8_standards_and_added_comments

--- odd_numbers_within_100.py
+++ odd_numbers_within_100.py
@@ -1,5 +1,12 @@
-number = 0  
-while number <= 100:
-    if number % 2 != 0:
-        print(number)
-    number += 1
+#Program 8 Batch 2
+#Atienza,Rein Gabriel
+#BSCPE 1-2
+def odd_numbers_within_100():
+    """prints the odd numbers that are present from 0 to 100"""
+    number = 0  
+    while number <= 100:
+        if number % 2 != 0:
+            print(number)
+        number += 1
+
+odd_numbers_within_100()
