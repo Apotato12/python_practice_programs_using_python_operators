ff2dfb571af784a303f480223a8c2718cbfb5a26
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-13 18:23:28 +0800

    fixed_pseudocode

--- unique_and_duplicate_numbers.py
+++ unique_and_duplicate_numbers.py
@@ -1,4 +1,5 @@
-#prompts the user to enter 10 numbers
+#prompts the user to enter numbers
 #checks the set if the number has already been entered
 #if the number has already been entered tag mark it as duplicate if not mark it as unique
 #print the numbers
+#if the number is invalid end the program
