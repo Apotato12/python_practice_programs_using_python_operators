9d7e169ee61c0e04969b0a3326e09adf894c02b6
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-13 09:11:50 +0800

    working_code

--- difference_of_2_numbers.py
+++ difference_of_2_numbers.py
@@ -1,3 +1,6 @@
-#asks the user to input 2 numbers
-#subtracts num 1 with num 2
-#prints their difference
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+difference = num1 - num2
+
+print (difference)
