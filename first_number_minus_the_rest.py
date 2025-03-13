964c3f951afcf3004ec8ca5cf39b4aea024af1be
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-13 13:23:21 +0800

    followed_PEP8_standards_and_added_comments

--- first_number_minus_the_rest.py
+++ first_number_minus_the_rest.py
@@ -1,8 +1,15 @@
-numbers = [float(input(f"Enter number {i + 1}: ")) for i in range(10)]
+#Program 6 Batch 2
+#Atienza,Rein Gabriel
+#BSCPE 1-2
+def first_minus_the_rest():
+    """"ask the user to input 10 numbers then seperate the first numbers with the other then get the difference"""
+    numbers = [float(input(f"Enter number {i + 1}: ")) for i in range(10)]
 
-first_number = numbers[0]
-rest_of_numbers = numbers[1:]
+    first_number = numbers[0]
+    rest_of_numbers = numbers[1:]
 
-result = first_number - sum(rest_of_numbers)
+    result = first_number - sum(rest_of_numbers)
 
-print("Result:", result)
+    print("Result:", result)
+
+first_minus_the_rest()
