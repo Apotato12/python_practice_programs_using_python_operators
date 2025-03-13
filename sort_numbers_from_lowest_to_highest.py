3db80ba073d63296dec21bf73faebd9da73bfb30
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-13 19:13:36 +0800

    working_code

--- sort_numbers_from_lowest_to_highest.py
+++ sort_numbers_from_lowest_to_highest.py
@@ -1,4 +1,15 @@
-#ask the user for numbers
-#make a set to store the numbers
-#sort the numbers
-#print the sorted numbers
+numbers = set()
+while True:
+        user_input = input("Enter a number (or type something invalid to stop): ")
+        try:
+            num = float(user_input)
+            numbers.add(num)
+        except ValueError:
+            print("Invalid input. Exiting the program.")
+            break
+
+sorted_numbers = sorted(numbers)
+
+print("Numbers from lowest to highest:")
+for number in sorted_numbers:
+    print(number)
