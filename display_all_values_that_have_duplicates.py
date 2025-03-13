cd1a070d1efca104dc12ccd5b98a5eb3f9ee75b3
Atienza, Rein Gabriel <atienza.reingabriel308129@gmail.com>
2025-03-13 19:58:06 +0800

    followed_PEP8_format_and_added_comments

--- display_all_values_that_have_duplicates.py
+++ display_all_values_that_have_duplicates.py
@@ -1,15 +1,22 @@
-numbers = set()
-duplicates = set()
+# Program 1 Batch 4
+# Atienza, Rein Gabriel
+# BSCPE 1-2
+def print_the_duplicates():
+    """Ask the user to input 10 numbers and display all numbers that have duplicates."""
+    numbers = set()
+    duplicates = set()
 
-print("Enter 10 numbers:")
-for i in range(10):
-     num = float(input("Number " + str(i + 1) + ": "))
+    print("Enter 10 numbers:")
+    for i in range(10):
+        num = float(input("Number " + str(i + 1) + ": "))
      
-     if num in numbers:
+        if num in numbers:
             duplicates.add(num)
-     else:
+        else:
             numbers.add(num)
 
-print("Numbers with duplicates:")
-for num in duplicates:
-    print(num)
+    print("Numbers with duplicates:")
+    for num in duplicates:
+        print(num)
+
+print_the_duplicates()
