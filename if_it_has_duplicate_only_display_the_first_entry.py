94d5bff0fc7e251fa7360f4590b2c4eb1de7ad28
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-13 18:04:20 +0800

    pseudocode

---
+++ if_it_has_duplicate_only_display_the_first_entry.py
@@ -0,0 +1,4 @@
+#prompt the user to input 10 numbers
+#make a set that tracks if numbers has already been entered
+#check if the number is in the set
+#if no print the number then add it to the tracker if yes don't print it
