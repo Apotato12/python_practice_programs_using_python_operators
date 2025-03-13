7ef4533f287e77b3ce17f679a475716e4a40d044
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-13 18:19:38 +0800

    pseudocode

---
+++ unique_and_duplicate_numbers.py
@@ -0,0 +1,4 @@
+#prompts the user to enter 10 numbers
+#checks the set if the number has already been entered
+#if the number has already been entered tag mark it as duplicate if not mark it as unique
+#print the numbers
