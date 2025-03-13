d97f316714e8be521c01965be5feaf6cfd66283a
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-13 12:08:54 +0800

    followed_PEP8_standards_and_added_comments

--- quotient_of_2_numbers_without_decimal.py
+++ quotient_of_2_numbers_without_decimal.py
@@ -1,6 +1,13 @@
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+#Program 4 Batch 2
+#Atienza,Rein Gabriel
+#BSCPE 1-2
+def quotient_of_the_2_numbers_without_decimal():
+    """prompts the user to enter 2 numbers and then divides them with each other to get the quotient without the decimal"""
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
 
-quotient = num1 / num2
+    quotient = num1 / num2
 
-print("the quotient of 2 numbers is:",int(quotient))
+    print("the quotient of 2 numbers is:",int(quotient) )
+
+quotient_of_the_2_numbers_without_decimal()
