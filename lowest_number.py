b9467f8c7c57689759d08aa187ebc8aacf95290e
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-13 19:01:40 +0800

    followed_PEP8_standards_and_added_comments

--- lowest_number.py
+++ lowest_number.py
@@ -1,6 +1,11 @@
-lowest_numbers = set()
+# Program 4 Batch 3
+# Atienza, Rein Gabriel
+# BSCPE 1-2
+def find_lowest_number():
+    lowest_numbers = set()
 
-while True:
+    while True:
+        """Ask the user to input numbers, store them in a set, and print the lowest number."""
         user_input = input("Enter a number (or type something invalid to stop): ")
         try:
             num = float(user_input)
@@ -8,8 +13,10 @@ while True:
         except ValueError:
             print("Invalid input. Exiting the program.")
             break
-if lowest_numbers:
+    if lowest_numbers:
         lowest_number = min(lowest_numbers) 
         print("The lowest number entered is:", lowest_number)
-else:
+    else:
         print("No valid numbers were entered.")
+
+find_lowest_number()
