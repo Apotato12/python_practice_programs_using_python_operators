a8338ab6be9110adea40c50fe07a612e8bb705a5
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-13 18:11:05 +0800

    working_code

--- if_it_has_duplicate_only_display_the_first_entry.py
+++ if_it_has_duplicate_only_display_the_first_entry.py
@@ -1,12 +1,20 @@
-list_of_numbers = []
+# Program 2 Batch 3
+# Atienza, Rein Gabriel
+# BSCPE 1-2
+def display_unique_numbers():
+    """Ask the user to input 10 numbers and display each number show the duplicates only once."""
+    list_of_numbers = []
 
-for i in range(10):
-    num = float(input("Number " + str(i + 1) + ": "))
-    list_of_numbers.append(num)
+    print("Enter 10 numbers:")
+    for i in range(10):
+        num = float(input("Number " + str(i + 1) + ": "))
+        list_of_numbers.append(num)
 
-tracker = set()
-print("Numbers (duplicates shown only once):")
-for num in list_of_numbers:
-    if num not in tracker:
-         print(num) 
-         tracker.add(num)
+    tracker = set()
+    print("Numbers (duplicates shown only once):")
+    for num in list_of_numbers:
+        if num not in tracker:
+            print(num)  
+            tracker.add(num)  
+
+display_unique_numbers()
