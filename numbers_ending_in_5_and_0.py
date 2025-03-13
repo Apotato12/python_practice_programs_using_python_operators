a94f8132627596d57bb9b9d8e0332ecaeb7b34ca
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-13 14:48:09 +0800

    working_code

--- numbers_ending_in_5_and_0.py
+++ numbers_ending_in_5_and_0.py
@@ -1,3 +1,3 @@
-#numbers from 0 to 100
-#remove the ones that does not end with 5 or 0
-#print the numbers
+for number in range(101):
+    if number % 10 == 0 or number % 10 == 5:
+     print(number)
