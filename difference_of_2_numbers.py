b4c4fe10732dc5181bd286d16623465b0c62e31a
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-13 09:18:47 +0800

    followed_PEP8_guidelines_and_added_comments

--- difference_of_2_numbers.py
+++ difference_of_2_numbers.py
@@ -1,6 +1,12 @@
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+#Program 3 batch 2
+#Atienza,Rein Gabriel
+#BSCPE 1-2
+def calculate_difference():
+    """asks the user to input 2 numbers and then subtracts them with each other"""
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    difference = num1 - num2
 
-difference = num1 - num2
+    print(difference)
 
-print (difference)
+calculate_difference()
