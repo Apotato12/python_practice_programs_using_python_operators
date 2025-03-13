e04d8e4b8ed51bffa350037c51682a1e322bcfbb
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-13 08:48:00 +0800

    pseudocode

---
+++ not_equal_numbers.py
@@ -0,0 +1,4 @@
+#asks the user to input 2 numbers
+#determine if the numbers are of equal value or not
+#if the values are equal print "that value is equal"
+#if the values are not equal print "num1 and num2 are not equal"
