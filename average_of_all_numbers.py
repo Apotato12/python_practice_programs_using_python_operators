8efc8bdf8f786b47fb4f57fa771f8d327b9aeec6
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-13 20:45:59 +0800

    working_code

--- average_of_all_numbers.py
+++ average_of_all_numbers.py
@@ -1,15 +1,15 @@
-numbers = set()
+average_numbers = set()
 
 while True:
     user_input = input("Enter a number (or type something invalid to stop): ")
         
     try:
         num = float(user_input)
-        numbers.add(num)
+        average_numbers.add(num)
     except ValueError:
         print("Invalid input. Exiting the program.")
         break
 
-if numbers:  
-    average = sum(numbers) / len(numbers) 
+if average_numbers:  
+    average = sum(average_numbers) / len(average_numbers) 
     print("The average of the entered numbers is:", average)
